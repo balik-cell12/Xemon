eec4431cc3580541016191e4cca926419b9fb8f3
KutsalBalık <localstorage64@gmail.com>
2026-04-13 21:21:18 +0300

    Create xemon.user.js

---
+++ xemon.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         test
+// @namespace    test
+// @version      0.1
+// @author       KutsalBalik
+// @match        *://*/*
+// @description  test
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+console("test")
+})();
